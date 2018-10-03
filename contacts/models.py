138680ad01cf0bedaf7633f468e4272f7c035921
Amit <rsamit26@gmail.com>
2018-10-04 04:57:32 +0530

    design added

--- contacts/models.py
+++ contacts/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 # Create your models here.
 
--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-
+app_name = 'contacts'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
